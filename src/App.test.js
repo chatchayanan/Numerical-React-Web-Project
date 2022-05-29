ad2f70294a7393b85e940c756a76ee5186766654
ชาติชญานันท์ มรรคณา <s6204062610016@email.kmutnb.ac.th>
2022-05-29 20:24:40 +0700

    delete app test

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('renders learn react link', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
